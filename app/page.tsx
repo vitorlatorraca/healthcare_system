a720a757665e52bad4f8589cc9e40f0bcddd465a
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-10-07 22:10:51 -0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image"; 
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+
+      /* TO DO */ 
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -21,10 +25,20 @@ export default function Home() {
             <p className="justify-items-end text-dark-600 xl:text-left">
             2024 CarePulse
             </p>
-            <Link href="/?admin=true"
+            <Link href="/?admin=true" className="text-green-500">
+            Admin
+            </Link>
           </div>
         </div>
       </section>
+
+      <Image
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
