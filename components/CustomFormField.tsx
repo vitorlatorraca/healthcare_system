11654556803f992673174058e63fe6818b99c92e
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-11-14 23:34:17 -0500

    new

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -12,6 +12,7 @@ import {
 import { Input } from "./ui/input"
 import { Control } from "react-hook-form"
 import { FormFieldType } from "./forms/PatientForm"
+import Image from 'next/image'
 
 interface CustomProps {
     control: Control<any>,
@@ -29,12 +30,33 @@ interface CustomProps {
 }
 
 const RenderField = ({field, props}: {field: any; props:CustomProps} ) => {
-    return (
-        <Input 
-        type="text"
-        placeholder="Enter your name"
-        />
-    )
+    const {fieldType, iconSrc, iconAlt, placeholder, } = props;
+
+    switch (fieldType) {
+        case FormFieldType.INPUT:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {iconSrc && (
+                        <Image 
+                            src={iconSrc}
+                            height={24}
+                            width={24}
+                            alt={iconAlt || 'icon'}
+                            className='ml-2'
+                        />
+                    )}
+                    <FormControl>
+                        <Input
+                        placeholder={placeholder}
+                        {...field}
+                        className="shad-input border-0"
+                        />
+                    </FormControl>
+                </div>
+            )
+            default:
+            break;
+        }
 }
 
 const CustomFormField = (props: CustomProps) => {
